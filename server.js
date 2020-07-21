ed669ac1176ab2fee51912c34b9f7e4a1d4fd36d
unknown <sangmins39@gmail.com>
2020-07-21 23:33:38 +0900

    필요없는 주석 제거
    데이터가 없을 때 사용하려고 만들어놓은 코드인데, 이제 더이상 필요없어서 삭제

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import methodOverride from "method-override";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import path from "path";
-import Post from "./models/Post.js";
 const __dirname =
   path.dirname(new URL(import.meta.url).pathname).slice(1) + "/";
 dotenv.config();
@@ -21,25 +20,6 @@ mongoose.connect("mongodb://localhost/test", {
 
 const db = mongoose.connection;
 db.once("open", async () => {
-  // if (await Post.countDocuments().exec() > 0) return;
-  // Promise.all([
-  //   Post.create({ title: "title1", body: "body1" }),
-  //   Post.create({ title: "title2", body: "body2" }),
-  //   Post.create({ title: "title3", body: "body3" }),
-  //   Post.create({ title: "title4", body: "body4" }),
-  //   Post.create({ title: "title5", body: "body5" }),
-  //   Post.create({ title: "title6", body: "body6" }),
-  //   Post.create({ title: "title7", body: "body7" }),
-  //   Post.create({ title: "title8", body: "body8" }),
-  //   Post.create({ title: "title9", body: "body9" }),
-  //   Post.create({ title: "title10", body: "body10" }),
-  //   Post.create({ title: "title11", body: "body11" }),
-  //   Post.create({ title: "title12", body: "body12" }),
-  //   Post.create({ title: "title13", body: "body13" }),
-  // ]).then(() => {
-  //   console.log("finish");
-  // });
-
   console.log("DB connected");
 });
 db.on("error", function (err) {
