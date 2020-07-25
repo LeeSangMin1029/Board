cd2e6fc224d6982b8e8a472cad1fb74b3851d221
unknown <sangmins39@gmail.com>
2020-07-25 09:24:37 +0900

    사용자 가입 기능 완성
    서버에는 경로에 대한 요청을 처리하는 간단한 로직 밖에 없다.
    가입 기능을 처리하는 로직을 보려면 DB쪽을 봐야한다.

---
+++ controller/users-controller.js
@@ -0,0 +1,33 @@
+import User from "../models/User.js";
+import * as util from "../utils.js";
+
+const getUsers = util.asyncWrap(async (req, res) => {
+  try {
+    const users = await User.find().lean();
+    return res.render("users/index", { users: users });
+  } catch (err) {
+    return res.send(err);
+  }
+});
+
+const renderRegisterForm = (req, res) => {
+  return res.render("users/register");
+};
+
+const createUser = util.asyncWrap(async (req, res) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      id: req.body.id,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirmation: req.body.passwordConfirmation,
+    });
+    await user.save();
+    return res.json({ redirect: true });
+  } catch (err) {
+    return res.json({ errors: util.errorHandler(err) });
+  }
+});
+
+export { getUsers, renderRegisterForm, createUser };
