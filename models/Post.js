b960dd107bdc44617e9765db161e745fc0972380
unknown <sangmins39@gmail.com>
2020-07-25 09:29:47 +0900

    메시지 변경
    통일성을 위해서 메시지 뒤에 '!'를 붙여주었다.

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-  title: { type: String, required: [true, "Title is required"] },
-  body: { type: String, required: [true, "Body is required"] },
+  title: { type: String, required: [true, "Title is required!"] },
+  body: { type: String, required: [true, "Body is required!"] },
   createdAt: { type: Date, default: Date.now },
   updatedAt: Date,
 });
