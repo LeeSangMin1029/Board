225a98790cc39dec0f74419209733eb22442b7aa
unknown <sangmins39@gmail.com>
2020-07-28 23:01:36 +0900

    로그인에 사용되는 로직
    이 모듈은 passport를 가져와서 세션에서 저장, 읽기를 가능하게 해준다.

---
+++ config/passport.js
@@ -0,0 +1,49 @@
+import passport from "passport";
+import passLocal from "passport-local";
+import User from "../models/User.js";
+
+passport.serializeUser(function (user, done) {
+  return done(null, user._id);
+});
+
+// request 요청시 매번 호출한다.
+passport.deserializeUser(async function (object_id, done) {
+  try {
+    const user = await User.findOne({ _id: object_id });
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});
+
+passport.use(
+  "local",
+  new passLocal.Strategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async function (email, password, done) {
+      let user;
+      let errors = {};
+      try {
+        user = await User.findOne({ email: email }).select({
+          password: 1,
+        });
+        if (!user) {
+          errors.email = { message: "Invalid email" };
+        } else {
+          if (!(await user.authenticate(password))) {
+            errors.password = { message: "Invalid password" };
+          }
+        }
+        return done(null, user, { errors: errors });
+      } catch (err) {
+        console.log(err);
+        return done(err);
+      }
+    }
+  )
+);
+
+export default passport;
