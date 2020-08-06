b9f52293ae7b334698f6a17937695c23b6c52e04
unknown <sangmins39@gmail.com>
2020-08-06 11:32:46 +0900

    change structure
    전체적으로 구조를 세분화시켰다. 하나의 기능을 수행하는 부분을 함수로 묶었다.

--- public/js/comment/EditFormValidation.js
+++ public/js/comment/EditFormValidation.js
@@ -1,34 +1,56 @@
-(async function () {
+function getEvents() {
+  return {
+    input: {
+      eventList: ["keyup", "paste", "input", "propertychange"],
+      callFn: inputCallFn,
+    },
+    btn: {
+      eventList: ["click"],
+      callFn: btnCallFn,
+    },
+    form: {
+      eventList: ["submit"],
+      callFn: formCallFn,
+    },
+  };
+}
+
+const inputCallFn = (e, doc) => {
+  const initalValue = doc.innerHTML;
+  if (initalValue == doc.innerHTML) {
+    console.log(doc.innerHTML);
+  }
+};
+
+const btnCallFn = (e, doc) => {
+  e.preventDefault();
+  const comment = doc.closest("#comment");
+  let toggleHidden = comment.querySelector("#comment-edit");
+  toggleHidden.hidden = toggleHidden.hidden ? false : true;
+  toggleHidden = comment.querySelector("#comment-body");
+  toggleHidden.hidden = toggleHidden.hidden ? false : true;
+};
+
+const formCallFn = async (e, doc) => {
   const { getData } = await import("../required/Request.js");
-  const { validateForm } = await import("../utils/FormValidation.js");
-  document.querySelectorAll("#action-edit, #edit-cancel").forEach((anchors) => {
-    anchors.addEventListener("click", async (e) => {
-      e.preventDefault();
-      const comment = anchors.closest("#comment");
-      let toggleHidden = comment.querySelector("#comment-edit");
-      toggleHidden.hidden = toggleHidden.hidden ? false : true;
-      toggleHidden = comment.querySelector("#comment-body");
-      toggleHidden.hidden = toggleHidden.hidden ? false : true;
-    });
-  });
-  document.querySelectorAll("form#edit").forEach((form) => {
-    form.addEventListener("submit", async (e) => {
-      e.preventDefault();
-      try {
-        const formdata = new FormData(form);
-        formdata.append("post_id", form.dataset.postId);
-        const { errors, redirect } = await getData(
-          `/comment/${form.dataset.commentId}`,
-          formdata,
-          "PUT"
-        );
-        if (typeof errors === "undefined" && redirect) {
-        } else {
-          validateForm(formdata, "comment edit", errors);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    });
-  });
+  e.preventDefault();
+  try {
+    const formdata = new FormData(doc);
+    formdata.append("post_id", doc.dataset.postId);
+    const { errors, redirect } = await getData(
+      `/comment/${doc.dataset.commentId}`,
+      formdata,
+      "PUT"
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+(async function () {
+  const { input, btn, form } = getEvents();
+  const { addEvent } = await import("../utils/FormValidation.js");
+  addEvent(".uit", input, true);
+  addEvent("#action-edit, #edit-cancel", btn, true);
+  addEvent("form#edit", form, true);
 })();
