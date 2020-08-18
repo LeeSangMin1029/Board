383418acd8128a7224739b36f07b47cb0c41f7c6
unknown <sangmins39@gmail.com>
2020-08-18 21:17:38 +0900

    Delete many files

--- public/js/user/EditFormValidation.js
+++
@@ -1,8 +0,0 @@
-(async function () {
-  const { getData } = await import("../required/Request.js");
-  const { validateForm } = await import("../utils/FormValidation.js");
-  const form = document.querySelector("form");
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-  });
-})();
