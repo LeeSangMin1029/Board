496cc29644b75ee8e4528e49fa479bfac3e78210
unknown <sangmins39@gmail.com>
2020-09-02 23:23:52 +0900

    검색 기능 추가
    npmjs.com의 검색 기능을 베껴왔다. 시멘틱 url에서 일반적인 query string 형식
    으로 변경했다.

--- src/public/js/post/RenderPosts.js
+++ src/public/js/post/RenderPosts.js
@@ -1,56 +1,169 @@
+import { getData } from "../required/Request.js";
 import { isEmpty } from "../utils/ObjectValidation.js";
 
-window.addEventListener("popstate", function () {
-  let page = this.location.pathname.split("/")[3];
-  if (isEmpty(page)) {
-    page = 1;
+const updateViewPage = async (queryObject, response) => {
+  try {
+    let resultQueryString = "";
+    const queryString = buildQueryString(queryObject);
+    if (!isEmpty(queryString)) {
+      resultQueryString += `?${queryString}`;
+    }
+    const { protocol, host, pathname } = window.location;
+    const newurl = `${protocol}//${host}${pathname}${resultQueryString}`;
+    history.pushState(null, "", newurl);
+    const getFromServerData = await getPosts(queryString);
+    response(getFromServerData);
+  } catch (err) {
+    console.log(err);
   }
-  document
-    .getElementsByClassName("current-page")[0]
-    .classList.remove("current-page");
-  document.querySelectorAll(".page")[page - 1].classList.add("current-page");
-  getPosts(page);
+};
+
+const btnClickEvent = async function () {
+  try {
+    currentPageToggle(this);
+    const queryObject = {};
+    const page = getPageNumber(this);
+    const searchText = getParameterByName("search");
+    if (!isEmpty(searchText)) {
+      queryObject.search = searchText;
+    }
+    queryObject.page = page;
+    updateViewPage(queryObject, ({ posts }) => {
+      renderPosts(posts);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const pageAllButtons = document.querySelectorAll(".page");
+pageAllButtons.forEach((btn) => {
+  btn.addEventListener("click", btnClickEvent);
 });
 
-document.querySelectorAll(".page").forEach((btn) => {
-  btn.addEventListener("click", async function (e) {
-    const page = btn.childNodes[0].textContent | 0;
-    const currentPage = document.getElementsByClassName("current-page");
-    if (!currentPage.length) {
-      this.classList.add("current-page");
+const searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const formData = new FormData(searchForm);
+    let searchText = formData.get("search");
+    const queryObject = {};
+    if (isEmpty(searchText)) {
+      searchText = "";
     } else {
-      currentPage[0].classList.remove("current-page");
-      this.classList.add("current-page");
-      await getPosts(page);
-      history.pushState(null, null, `/posts/page/${page}`);
+      queryObject.search = searchText;
     }
-  });
+    updateViewPage(queryObject, ({ posts, count, currentPage }) => {
+      renderPosts(posts);
+      pageAllButtons.forEach((btn) => {
+        btn.removeEventListener("click", btnClickEvent);
+      });
+      renderPageList(count, currentPage);
+      document.querySelectorAll(".page").forEach((btn) => {
+        btn.addEventListener("click", btnClickEvent);
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+window.addEventListener("popstate", async function (e) {
+  try {
+    let page = getParameterByName("page");
+    let searchText = getParameterByName("search");
+    const queryObject = {};
+    if (isEmpty(page)) {
+      page = 1;
+    }
+    if (isEmpty(searchText)) {
+      searchText = "";
+    } else {
+      queryObject.search = searchText;
+    }
+    queryObject.page = page;
+    const prevClickedPage = document.querySelectorAll(".page")[page - 1];
+    currentPageToggle(prevClickedPage);
+    const queryString = buildQueryString(queryObject);
+    const { posts, count, currentPage } = await getPosts(queryString);
+    renderPosts(posts);
+    renderPageList(count, currentPage);
+    document.querySelectorAll(".page").forEach((btn) => {
+      btn.addEventListener("click", btnClickEvent);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+const currentPageToggle = (btnElements) => {
+  try {
+    document.querySelector(".current-page").classList.remove("current-page");
+    btnElements.classList.add("current-page");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getPageNumber = (btn) => {
+  return btn.querySelector(":scope > .num-page").textContent;
+};
+
+const getParameterByName = (name, url = window.location.href) => {
+  const replacedName = name.replace(/[\[\]]/g, "\\$&");
+  const regex = new RegExp(`[?&]${replacedName}(=([^&#]*)|&|#|$)`),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
+const buildQueryString = (params) => {
+  if (isEmpty(params)) {
+    return null;
+  }
+  return Object.keys(params)
+    .map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`)
+    .join("&");
+};
+
+const renderPageList = (count, currentPage) => {
+  try {
+    let result = "";
+    for (let i = 1; i <= count; i++) {
+      if (currentPage == i)
+        result += `<button class="page current-page"><em class="num-page">${i}</em></button>`;
+      else
+        result += `<button class="page"><em class="num-page">${i}</em></button>`;
+    }
+    document.querySelector(".page-wrap").innerHTML = result;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 function renderErrorMsg(err) {
   document.getElementById("tbody-posts").innerHTML = `<tr>${err}</tr>`;
 }
 
 function renderPosts(posts) {
-  let result = "";
-  for (const post of posts) {
-    result += `<tr><td><a href="/posts/${post._id}" class="txt-hidden">${post.title}</a></td>
-      <td>${post.author.name}</td>
-      <td>${post.createdAt}</td></tr>`;
-  }
-  document.getElementById("tbody-posts").innerHTML = result;
+  try {
+    let result = "";
+    for (const post of posts) {
+      result += `<tr><td><a href="/posts/${post._id}" class="txt-hidden">${post.title}</a></td>
+        <td>${post.author.name}</td>
+        <td>${post.createdAt}</td></tr>`;
+    }
+    document.getElementById("tbody-posts").innerHTML = result;
+  } catch (err) {}
 }
 
-async function getPosts(page) {
+async function getPosts(queryString = "") {
   try {
-    const { getData } = await import("../required/Request.js");
-    const { posts } = await getData(`/posts/page/${page}`);
-    if (isEmpty(posts)) {
-      renderErrorMsg(`There are no posts please create it`);
-    } else {
-      renderPosts(posts);
+    let requestPath = "";
+    if (!isEmpty(queryString)) {
+      requestPath = `?${queryString}`;
     }
-  } catch (err) {
-    console.log(err);
-  }
+    return await getData(`/posts${requestPath}`);
+  } catch (err) {}
 }
