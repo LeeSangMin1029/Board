777463c2a1abbda182c820f9981ea0c8f7a53778
unknown <sangmins39@gmail.com>
2020-09-06 17:15:33 +0900

    에러 메시지 함수 생성 및 이벤트 추가하는 함수 생성

--- src/public/js/utils/Doc.js
+++ src/public/js/utils/Doc.js
@@ -10,8 +10,47 @@ const getDocuments = (selector = "", isAll = false) => {
   } catch (err) {}
 };
 
-const showAlert = (target, errors) => {
-  console.log(target, errors);
+const addEvent = function (
+  doc,
+  eventName = "",
+  bindingFn = {},
+  prevent = false,
+  useCapture = false
+) {
+  let execute = {};
+  if (typeof bindingFn === "function") execute = bindingFn.bind(this);
+  else throw new Error("This bindingFn is not a function!");
+  doc.addEventListener(
+    eventName,
+    (e) => {
+      prevent ? e.preventDefault() : null;
+      try {
+        execute(e, doc);
+      } catch (err) {}
+    },
+    useCapture
+  );
 };
 
-export { getDocuments, showAlert, isEmpty, isNotEmpty };
+const alertError = (targetDocument = {}, errorMsg = "", attrName = "") => {
+  try {
+    targetDocument.setAttribute(attrName, errorMsg);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const alertAllError = (errors, attrName) => {
+  for (const [k, v] of Object.entries(errors)) {
+    alertError(getDocuments(`#${k}`).parentNode, v, attrName);
+  }
+};
+
+export {
+  getDocuments,
+  addEvent,
+  alertError,
+  alertAllError,
+  isEmpty,
+  isNotEmpty,
+};
