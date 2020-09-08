4446b2479c87b8cdb6df9c50e79d08d29d91f117
unknown <sangmins39@gmail.com>
2020-09-09 03:32:58 +0900

    로그인 폼 검사 수정

--- src/public/js/user/LoginFormValidation.js
+++ src/public/js/user/LoginFormValidation.js
@@ -1,16 +1,39 @@
-(async function () {
-  const { getData } = await import("../required/Request.js");
-  const form = document.querySelector("form");
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    try {
-      const formdata = new FormData(form);
-      const { errors, response } = await getData("/login", formdata, "POST");
-      if (typeof errors === "undefined" && response) {
-        window.location.href = "/";
-      }
-    } catch (err) {
-      console.log(err);
+import { getDocuments, addEvent } from "../utils/Doc.js";
+import { navigateToURL, partial } from "../utils/Util.js";
+
+const loginFormSubmit = async function () {
+  try {
+    const formData = new FormData(this);
+    const fetched = await fetch("/login", {
+      body: formData,
+      method: "POST",
+    });
+    const {
+      response: { success = false, errors = {} },
+    } = await fetched.json();
+    if (success) {
+      navigateToURL("/");
+    } else {
+      this.querySelectorAll(".input-area").forEach((input) => {
+        if (input.hasAttribute("error-messages")) {
+          input.removeAttribute("error-messages");
+        }
+      });
+      Object.keys(errors).map((key) => {
+        const input = this.querySelector(`#${key}`);
+        input.parentNode.setAttribute("error-messages", errors[key]);
+      });
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+(() => {
+  try {
+    const loginForm = getDocuments("#user-login");
+    addEvent(loginForm, "submit", partial(loginFormSubmit), true);
+  } catch (err) {
+    console.log(err);
+  }
 })();
