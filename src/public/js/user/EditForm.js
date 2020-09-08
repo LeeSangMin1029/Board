1501d2d24a4639a6c4df17baf8abd712210688bb
unknown <sangmins39@gmail.com>
2020-09-09 05:49:37 +0900

    사용자 수정을 위한 모듈 이름 변경 및 이전에 사용했던 모듈 제거

---
+++ src/public/js/user/EditForm.js
@@ -0,0 +1,40 @@
+import { getDocuments, addEvent } from "../utils/Doc.js";
+import { navigateToURL, partial } from "../utils/Util.js";
+
+const editFormSubmit = async function () {
+  try {
+    const formData = new FormData(this);
+    const fetched = await fetch(this.action, {
+      body: formData,
+      method: "POST",
+    });
+    const {
+      response: { success = false, errors = {} },
+    } = await fetched.json();
+    if (success) {
+      console.log(location);
+      navigateToURL("/");
+    } else {
+      this.querySelectorAll(".input-area").forEach((input) => {
+        if (input.hasAttribute("error-messages")) {
+          input.removeAttribute("error-messages");
+        }
+      });
+      Object.keys(errors).map((key) => {
+        const input = this.querySelector(`#${key}`);
+        input.parentNode.setAttribute("error-messages", errors[key]);
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+(() => {
+  try {
+    const editForm = getDocuments("#user-edit");
+    addEvent(editForm, "submit", partial(editFormSubmit), true);
+  } catch (err) {
+    console.log(err);
+  }
+})();
