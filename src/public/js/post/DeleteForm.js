897b3f99aa97e2587b64fdbb7eadc0e86a83163f
unknown <sangmins39@gmail.com>
2020-09-09 18:46:13 +0900

    글 삭제시 폼 검사추가

---
+++ src/public/js/post/DeleteForm.js
@@ -0,0 +1,32 @@
+import { getDocuments, addEvent } from "../utils/Doc.js";
+import { navigateToURL, partial } from "../utils/Util.js";
+
+const deletePostFormSubmit = async function (form) {
+  if (!confirm("Do you want delete this post?")) return;
+  try {
+    const formData = new FormData(form);
+    const fetched = await fetch(form.action, {
+      body: formData,
+      method: "POST",
+    });
+    const {
+      response: { success = true },
+    } = await fetched.json();
+    if (success) {
+      navigateToURL(location);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+(() => {
+  const deleteForm = getDocuments("#post-delete");
+  if (!deleteForm) return;
+  addEvent(
+    deleteForm.childNodes[0],
+    "click",
+    partial(deletePostFormSubmit, deleteForm),
+    true
+  );
+})();
