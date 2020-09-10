133dc35a128bab5b5eecb839fd4dff88ace8e780
unknown <sangmins39@gmail.com>
2020-09-11 03:43:06 +0900

    재사용 가능한 함수 추가

--- src/public/js/utils/Util.js
+++ src/public/js/utils/Util.js
@@ -1,4 +1,4 @@
-import { isNotEmpty } from "./ObjectValidation.js";
+import { isEmpty, isNotEmpty } from "./ObjectValidation.js";
 
 const navigateToURL = (targetURL = "") => {
   try {
@@ -6,7 +6,7 @@ const navigateToURL = (targetURL = "") => {
       location = targetURL;
     }
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
 
@@ -16,4 +16,41 @@ const partial = function (func, ...argsBound) {
   };
 };
 
-export { navigateToURL, partial };
+const getParameterByName = (name, url = window.location.href) => {
+  const replacedName = name.replace(/[\[\]]/g, "\\$&");
+  const regex = new RegExp(`[?&]${replacedName}(=([^&#]*)|&|#|$)`),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
+const buildQueryString = (params = {}) => {
+  try {
+    const query = filterEmptyValue(params);
+    if (isEmpty(query)) return "";
+    return `?${new URLSearchParams(query)}`;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const filterEmptyValue = (object = {}) => {
+  try {
+    const filteredObject = Object.keys(object).reduce((acc, key) => {
+      if (!isEmpty(object[key])) acc[key] = object[key];
+      return acc;
+    }, {});
+    return filteredObject;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export {
+  navigateToURL,
+  getParameterByName,
+  buildQueryString,
+  filterEmptyValue,
+  partial,
+};
